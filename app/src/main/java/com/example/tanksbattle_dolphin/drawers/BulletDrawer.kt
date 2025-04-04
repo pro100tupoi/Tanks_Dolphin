9afa5305b63696473b3fff6a303e9eec05de1980
pro100tupoi <mrpeps1@mail.ru>
2025-04-05 01:25:55 +0300

    10 presintation well done

--- app/src/main/java/com/example/tanksbattle_dolphin/drawers/BulletDrawer.kt
+++ app/src/main/java/com/example/tanksbattle_dolphin/drawers/BulletDrawer.kt
@@ -62,8 +62,8 @@ class BulletDrawer(val container: FrameLayout) {
 
     private fun chooseBehaviorInTermsOfDirections(
         elementsOnContainer: MutableList<Element>,
-    currentDirection: Direction,
-    bulletCoordinate: Coordinate
+        currentDirection: Direction,
+        bulletCoordinate: Coordinate
     ) {
         when (currentDirection) {
             Direction.DOWN, Direction.UP -> {
@@ -76,18 +76,13 @@ class BulletDrawer(val container: FrameLayout) {
         }
     }
 
-    private fun compareCollections(elementsOnContainer: MutableList<Element>,
-    detectedCoordinatesList: List<Coordinate>
+    private fun compareCollections(
+        elementsOnContainer: MutableList<Element>,
+        detectedCoordinatesList: List<Coordinate>
     ) {
-        if (checkContainerContainsElements(
-                elementsOnContainer.map { it.coordinate },
-                detectedCoordinatesList
-            )
-        ) {
-            detectedCoordinatesList.forEach {
-                val element = getElementByCoordinates(it, elementsOnContainer)
-                removeElementsAndStopBullet(element, elementsOnContainer)
-            }
+        detectedCoordinatesList.forEach {
+            val element = getElementByCoordinates(it, elementsOnContainer)
+            removeElementsAndStopBullet(element, elementsOnContainer)
         }
     }
 
@@ -95,9 +90,22 @@ class BulletDrawer(val container: FrameLayout) {
         element: Element?,
         elementsOnContainer: MutableList<Element>
     ) {
+        if (element != null) {
+            if (element.material.bulletCanGoThrough) {
+                return
+            }
+            if (element.material.simpleBulletCanDestroy) {
+                stopBullet()
+                removeView(element)
+                elementsOnContainer.remove(element)
+            } else {
+                stopBullet()
+            }
+        }
+    }
+
+    private fun stopBullet() {
         canBulletGoFurther = false
-        removeView(element)
-        elementsOnContainer.remove(element)
     }
 
     private fun removeView(element: Element?) {
@@ -107,18 +115,6 @@ class BulletDrawer(val container: FrameLayout) {
                 container.removeView(activity.findViewById(element.viewId))
             }
         }
-    }
-
-    private fun checkContainerContainsElements(
-        elementsOnContainer: List<Coordinate>,
-        detectedCoordinatesList: List<Coordinate>
-    ): Boolean {
-        detectedCoordinatesList.forEach{
-            if (elementsOnContainer.contains(it)) {
-                return true
-            }
-        }
-        return false
     }
 
     private fun getCoordinatesForTopOrBottomDirection(bulletCoordinate: Coordinate): List<Coordinate> {
